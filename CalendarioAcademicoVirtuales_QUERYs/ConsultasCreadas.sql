f228bab72433dfdd67d0ff6f2c14a79755b6f501
Fabio Ramos <fabio.ramos@outlook.es>
2020-02-15 12:01:00 -0600

    Se ordeno los metodos de alumnos solventes e insolventes

--- CalendarioAcademicoVirtuales_QUERYs/ConsultasCreadas.sql
+++ CalendarioAcademicoVirtuales_QUERYs/ConsultasCreadas.sql
@@ -1,4 +1,5 @@
-SELECT [caa_codigo], [caa_dias], [caa_hora], CONVERT(VARCHAR,[caa_fecha],103) caa_fecha, [caa_evaluacion], [caa_grupo] 
-FROM [web_ra_caa_calendario_acad]
+SELECT caa_codigo, caa_dias, caa_hora, CONVERT(VARCHAR,caa_fecha,103) caa_fecha, 
+caa_evaluacion, caa_grupo 
+from web_ra_caa_calendario_acad
 
 order by caa_grupo asc, caa_evaluacion asc
---
+++ ReporteOlmedo_QUERYs.sql
@@ -0,0 +1,6 @@
+select * from ra_pla_planes where pla_estado = 'A' and pla_tipo = 'U'
+select * from ra_plm_planes_materias_pensum
+
+---	Número de alumnos que llevan en orden  el pensum (desde el ciclo que ingreso a la universidad)
+---	Número de alumnos que no llevan en orden el pensum
+---	Identificar la cantidad de materias que se van a servir por primera vez en el ciclo 02-2020 y así sucesivamente. (por escuela)
