63bed95984d747bab946a3e8b8f2db0c6274dadc
Ado595 <49847204+Ado595@users.noreply.github.com>
2020-02-17 15:17:42 -0600

    Clean

--- CalendarioAcademicoVirtuales_QUERYs/ConsultasCreadas.sql
+++ CalendarioAcademicoVirtuales_QUERYs/ConsultasCreadas.sql
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-SELECT caa_codigo, caa_dias, caa_hora, CONVERT(VARCHAR,caa_fecha,103) caa_fecha, 
-caa_evaluacion, caa_grupo 
-from web_ra_caa_calendario_acad
-=======
---SELECT [caa_codigo], [caa_dias], [caa_hora], CONVERT(VARCHAR,[caa_fecha],103) caa_fecha, [caa_evaluacion], [caa_grupo] 
---FROM [web_ra_caa_calendario_acad]
->>>>>>> refs/remotes/origin/master
-
---order by caa_grupo asc, caa_evaluacion asc
 
 create table web_caav_calendario_acad_virtual(
 	caav_codigo int primary key identity(1,1),
