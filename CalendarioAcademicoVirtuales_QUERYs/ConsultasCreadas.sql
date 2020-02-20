492680c64c619433c70309a9bde31f938e7c0072
Ado595 <49847204+Ado595@users.noreply.github.com>
2020-02-20 08:58:06 -0600

    Se agrego la columna caav_bloque para manejar bloques de fechas por evaluacion.

--- CalendarioAcademicoVirtuales_QUERYs/ConsultasCreadas.sql
+++ CalendarioAcademicoVirtuales_QUERYs/ConsultasCreadas.sql
@@ -5,6 +5,7 @@ create table web_caav_calendario_acad_virtual(
 	caav_codpla int,
 	caav_evaluacion int,
 	caav_fecha_limite datetime,
+	caav_bloque int,
 	caav_usuario int,
 	caav_fecha_registro datetime default getdate()
 )
